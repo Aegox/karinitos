26aba4b58d86bbd36111d26e7ab086c582d57ef2
Diego Amundaray <diegoamundaray2017@gmail.com>
2023-10-20 08:40:20 -0500

    add Landing.jsx structure

---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+const Landing = () => {
+    return (
+        <div>
+            <section>
+                <article>
+                    <img>
+                    <span></span>
+                </article>
+                <img/>
+            </section>
+        </div>
+    )
+};
+
+export default Landing;
